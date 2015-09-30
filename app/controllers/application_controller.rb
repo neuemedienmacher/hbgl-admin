d1436f09b183e1e729d9704e60011b6214787e9d
Konstantin K <konstantin.koss@gmail.com>
2015-09-30 15:46:34 +0200

    enable http password protection on staging and production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # HTTP password protection
+  claradmin = Rails.application
+  http_basic_authenticate_with(
+    name: claradmin.secrets.protect['user'],
+    password: claradmin.secrets.protect['pwd'],
+    if: -> { Rails.env.staging? || Rails.env.production? }
+  )
 end
