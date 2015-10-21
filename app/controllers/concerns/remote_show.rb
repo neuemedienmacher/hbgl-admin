7936292b9a7e279d3ac86315c0ad1a053c54fc2e
Konstantin K <konstantin.koss@gmail.com>
2015-10-21 17:46:33 +0200

    fix for clarat-org/clarat#508 - link to the new preview from backend

--- app/controllers/concerns/remote_show.rb
+++ app/controllers/concerns/remote_show.rb
@@ -1,6 +1,6 @@
 module RemoteShow
   def redirect_to_remote_show controller
     host = Rails.application.secrets.frontend_host
-    redirect_to "#{host}/#{controller}/#{params[:id]}"
+    redirect_to "#{host}/preview/#{controller}/#{params[:id]}"
   end
 end
