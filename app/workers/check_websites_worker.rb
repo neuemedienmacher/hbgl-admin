2c6435f3e79b4e125adb42ac5575af253f5cbf7a
Nils Vollmer <nils_vollmer@gmx.de>
2015-11-19 11:29:29 +0100

    clarat-org/clarat#547 added heavy_duty queue and and made some worker changes

--- app/workers/check_websites_worker.rb
+++ app/workers/check_websites_worker.rb
@@ -4,7 +4,8 @@ class CheckWebsitesWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence { weekly(1).day(:sunday).hour_of_day(23) }
+  sidekiq_options queue: :heavy_load
+  recurrence { weekly(1).day(:monday).hour_of_day(1) }
 
   def perform
     # Get websites to check (only those with approved offers)
@@ -13,11 +14,14 @@ class CheckWebsitesWorker
     return if websites.empty?
 
     # Check websites for invalids and get affected offers
-    affected_offers = check_websites_and_get_affected_offers websites
+    affected_offers = []
+    websites.each do |website|
+      affected_offers.push(*check_website_and_get_affected_offers(website))
+    end
 
     # Create Asana Tasks, set state to expired and manually reindex for algolia
     asana = AsanaCommunicator.new
-    affected_offers.each do |expiring_offer|
+    affected_offers.compact.each do |expiring_offer|
       asana.create_expire_task expiring_offer, '[URL unreachable]'
       expiring_offer.update_columns(aasm_state: 'expired')
       expiring_offer.index!
@@ -26,21 +30,16 @@ class CheckWebsitesWorker
 
   private
 
-  def check_websites_and_get_affected_offers websites
-    affected_offers = []
-    websites.each do |website|
-      # get website and check for error codes
-      begin
-        response = HTTParty.get(website.url)
-        if !response || response.code >= 400 # everything above 400 is an error
-          affected_offers.push(*website.offers.approved.to_a)
-        end
-      # catch errors that prevent a valid response
-      rescue HTTParty::RedirectionTooDeep, Errno::EHOSTUNREACH, SocketError,
-             Timeout::Error
-        affected_offers.push(*website.offers.approved.to_a)
-      end
+  def check_website_and_get_affected_offers website
+    # get website and check for error codes
+    response = HTTParty.get(website.url)
+    if !response || response.code >= 400 # everything above 400 is an error
+      return website.offers.approved.to_a
     end
-    affected_offers.compact
+    []
+  # catch errors that prevent a valid response
+  rescue HTTParty::RedirectionTooDeep, Errno::EHOSTUNREACH, SocketError,
+         Timeout::Error
+    return website.offers.approved.to_a
   end
 end
