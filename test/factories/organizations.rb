d39f2b0fe31de9a0418a0d4f3cac143c9bda90c4
Nils Vollmer <nils_vollmer@gmx.de>
2015-12-18 10:52:02 +0100

    clarat-org/clarat#537 back to clarat_base master and changed orga factory

--- test/factories/organizations.rb
+++ test/factories/organizations.rb
@@ -28,13 +28,10 @@ FactoryGirl.define do
 
     after :create do |orga, evaluator|
       evaluator.website_count.times do
-        # create website with faked name, then set it to host 'own' and assign it
-        website = FactoryGirl.create(:website)
-        website.update_columns(host: 'own')
+        website = FactoryGirl.create(:website, host: 'own')
+        website.organizations << orga
         orga.websites << website
-        website.organizations = [orga]
       end
-
       # Locations
       if evaluator.location_count > 0
         orga.locations << FactoryGirl.create(:location, :hq, organization: orga)
