0b548a76d19fd9b489113e320ef5443d14ccfcad
Nils Vollmer <nils_vollmer@gmx.de>
2016-02-02 16:21:53 +0100

    clarat-org/clarat#652 _base update for new trait filter and adjusted offer.duplicate

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -30,11 +30,13 @@ class Offer < ActiveRecord::Base
       offer.section_filters = self.section_filters
       offer.language_filters = self.language_filters
       offer.target_audience_filters = self.target_audience_filters
+      offer.trait_filters = self.trait_filters
       offer.websites = self.websites
       offer.contact_people = self.contact_people
       offer.keywords = self.keywords
       offer.area = self.area
       offer.aasm_state = 'initialized'
+      offer.code_word = ''
     end
   end
   # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
