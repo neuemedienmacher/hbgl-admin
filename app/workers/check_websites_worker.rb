700127f1abe572f7baceaaf7b05e2022b0c6f481
Nils Vollmer <nils_vollmer@gmx.de>
2016-02-04 16:06:17 +0100

    update _base for lower sidekiq and sidetiq version

--- app/workers/check_websites_worker.rb
+++ app/workers/check_websites_worker.rb
@@ -1,14 +1,14 @@
 class CheckWebsitesWorker
   include Sidekiq::Worker
-  # include Sidetiq::Schedulable
+  include Sidetiq::Schedulable
 
   sidekiq_options queue: :heavy_load
-  # recurrence { weekly.day(3).hour_of_day(20) }
+  recurrence { weekly.day(3).hour_of_day(20) }
 
   def perform
     # Get websites to check (only those with approved offers or orgas)
-    # Website.select { |w| !w.offers.approved.empty? || !w.organizations.approved.empty? }.each do |website|
-    #   CheckSingleWebsiteWorker.perform_async website.id
-    # end
+    Website.select { |w| !w.offers.approved.empty? || !w.organizations.approved.empty? }.each do |website|
+      CheckSingleWebsiteWorker.perform_async website.id
+    end
   end
 end
