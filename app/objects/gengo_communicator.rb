8a5c1085cf82890898b8a3e233bd25232c235b0c
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2016-05-26 12:10:19 +0200

    Add instructions for gengo translators

--- app/objects/gengo_communicator.rb
+++ app/objects/gengo_communicator.rb
@@ -19,10 +19,11 @@ class GengoCommunicator
         tier: TIER, type: 'text', max_chars: 255, auto_approve: 1,
         lc_src: 'en', lc_tgt: locale.to_s,
         body_src: model.send("#{field}_en"),
-        slug: "#{model.class.name}:#{model.id}:#{field}_#{locale}"
-        # comment
-        # purpose
-        # tone
+        slug: "#{model.class.name}:#{model.id}:#{field}_#{locale}",
+        comment:  "It's for a web app to help refugees in Germany to find help.
+                  Please use a very simple / easy to understand language"
+        purpose:  "Web content. Describing help for refugees in Germany",
+        tone:     "Friendly. Please use easy to understand language."
       }
     end
 
