2bc105fdbec7ff5ba8b5ba536813d8dc5ba68bf0
Nils Vollmer <nils_vollmer@gmx.de>
2016-06-02 12:09:37 +0200

    minor changes to split_base

--- test/models/split_base_test.rb
+++ test/models/split_base_test.rb
@@ -6,8 +6,8 @@ describe SplitBase do
     describe '#display_name' do
       it 'should return correct name' do
         split_bases(:basic).display_name.must_equal(
-          'foobar - basicSolutionCategoryName - '\
-          'basicSplitBaseTitle - basicSplitBaseClaratAddition'
+          'foobar || basicSolutionCategoryName || '\
+          'basicSplitBaseTitle || basicSplitBaseClaratAddition'
         )
       end
     end
