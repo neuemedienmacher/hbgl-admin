efa10f8f46f1bbd088f2739e0b1a7771ba563aa7
Nils Vollmer <nils_vollmer@gmx.de>
2016-06-16 13:24:14 +0200

    mini-feature for clarat-org/clarat#833 - add ID to SplitBase display_name

--- app/models/split_base.rb
+++ app/models/split_base.rb
@@ -7,7 +7,7 @@ class SplitBase < ActiveRecord::Base
   delegate :name, to: :solution_category, prefix: true, allow_nil: true
 
   def display_name
-    "#{organization_name} || #{title} || #{solution_category_name} ||"\
+    "##{id} #{organization_name} || #{title} || #{solution_category_name} ||"\
       " #{clarat_addition}"
   end
 end
