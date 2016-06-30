b71f75390d1d614d5b4a3d4488c6566bd1b6f2d5
Konstantin K <konstantin.koss@gmail.com>
2016-06-30 13:08:39 +0200

    for clarat-org/clarat#815 - add translation links to old backend view

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -34,7 +34,7 @@ RailsAdmin.config do |config|
     Category Email UpdateRequest LanguageFilter User Contact
     Keyword Definition Note Area SearchLocation ContactPerson
     Subscription SectionFilter NextStep SolutionCategory
-    LogicVersion SplitBase OfferTranslation
+    LogicVersion SplitBase
   )
 
   config.actions do
@@ -358,6 +358,30 @@ RailsAdmin.config do |config|
       end
       field :created_by
       field :approved_by
+      field :translation_links do
+        formatted_value do
+          en = bindings[:object].translations.where(locale: :en).first
+          ar = bindings[:object].translations.where(locale: :ar).first
+          ru = bindings[:object].translations.where(locale: :ru).first
+          output_string = ''
+          output_string += if en
+            "<a href='/offer_translations/#{en.id}/edit'>Englisch</a><br/>"
+          else
+            'Englisch (wird noch erstellt)<br/>'
+          end
+          output_string += if en
+            "<a href='/offer_translations/#{ar.id}/edit'>Arabisch</a><br/>"
+          else
+            'Arabisch (wird noch erstellt)<br/>'
+          end
+          output_string += if en
+            "<a href='/offer_translations/#{ru.id}/edit'>Russisch</a><br/>"
+          else
+            'Russisch (wird noch erstellt)'
+          end
+          output_string.html_safe
+        end
+      end
     end
 
     clone_config do
@@ -366,21 +390,6 @@ RailsAdmin.config do |config|
 
     export do
       field :id
-    end
-  end
-
-  config.model 'OfferTranslation' do
-    weight(-3)
-    field :locale do
-      read_only true
-    end
-    field :name
-    field :description
-    field :opening_specification
-    field :source
-
-    field :offer do
-      read_only true
     end
   end
 
