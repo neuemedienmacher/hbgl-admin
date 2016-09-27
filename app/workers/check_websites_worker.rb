940aa6d980293ba713847d1d69fcbff1dfbd3b07
Nils Vollmer <nils_vollmer@gmx.de>
2016-09-27 14:08:25 +0200

    Fix for clarat-org/clarat#925 - enable crawler for Facebook websites (works very well on current data set)

--- app/workers/check_websites_worker.rb
+++ app/workers/check_websites_worker.rb
@@ -5,8 +5,7 @@ class CheckWebsitesWorker
   sidekiq_options queue: :heavy_load
 
   def perform
-    # ignore facebook because it seems to block the crawler leading to false positives
-    Website.where.not(host: 'facebook').find_each do |website|
+    Website.find_each do |website|
       next unless website.unreachable? || website.offers.approved.any? ||
                   website.organizations.approved.any?
       CheckSingleWebsiteWorker.perform_async website.id
