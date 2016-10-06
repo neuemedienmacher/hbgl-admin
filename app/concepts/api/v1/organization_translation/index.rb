eb221320963acbd2c2ea59c1e076367830dbbb1e
Nils Vollmer <nils_vollmer@gmx.de>
2016-10-06 15:22:53 +0200

    Fixed duplicate selection of OrganizationTranslations (via joins section_filters) and field errors while loading after switching index models
    also increased the fade-out effect while reloading table cells

--- app/concepts/api/v1/organization_translation/index.rb
+++ app/concepts/api/v1/organization_translation/index.rb
@@ -3,7 +3,7 @@ module API::V1
   module OrganizationTranslation
     class Index < API::V1::Default::Index
       def base_query
-        ::OrganizationTranslation.where(locale: [:en, :ar, :fa])
+        ::OrganizationTranslation.where(locale: [:en, :ar, :fa]).uniq
           .joins(:section_filters).where('filters.identifier = ?', 'refugees')
       end
 
