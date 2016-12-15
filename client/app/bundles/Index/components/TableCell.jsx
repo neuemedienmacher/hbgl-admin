4995a4c16247bb1002bf958abcce48cc78e71f61
Nils Vollmer <nils_vollmer@gmx.de>
2016-12-15 11:00:02 +0100

    HOTFIX: fixed two bugs in TableCell Rendering and added a hint when too long texts are trimmed in tableViews

--- client/app/bundles/Index/components/TableCell.jsx
+++ client/app/bundles/Index/components/TableCell.jsx
@@ -15,31 +15,38 @@ export default class TableCell extends Component {
   }
 
   renderContent(content, contentType) {
-    switch(contentType) {
-      case 'object':
-        if(isArray(content)){
-          return(
-            <p>
-              {content.map(obj => this.renderContent(obj)).join(', ')}
-            </p>
-          )
-        } else if(content.label) {
-          return content.label
-        } else {
+    if (!content) {
+      return null
+    }
+    else{
+      switch(contentType) {
+        case 'object':
+          if(isArray(content)){
+            return(
+              <p>
+                {content.map(obj =>
+                  this.renderContent(obj, obj.type)
+                ).join(', ')}
+              </p>
+            )
+          } else if(content.label) {
+            return content.label
+          } else {
+            return content
+          }
+        case 'boolean':
+          if (content) {
+            return <span className='fui-check' title='Ja' />
+          } else {
+            return <span className='fui-cross' title='Nein' />
+          }
+        case 'string':
+          return content.substr(0, 100) + (content.length > 100 ? ' (...)' : '')
+        case 'time':
+          return new Date(content).toLocaleString('de-de')
+        default:
           return content
-        }
-      case 'boolean':
-        if (content) {
-          return <span className='fui-check' title='Ja' />
-        } else {
-          return <span className='fui-cross' title='Nein' />
-        }
-      case 'string':
-        return content.substr(0, 100)
-      case 'time':
-        return new Date(content).toLocaleString('de-de')
-      default:
-        return content
+      }
     }
   }
 }
