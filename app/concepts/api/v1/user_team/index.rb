93cf8252e57d9092987a5d5fabe828f186f9e77d
Konstantin K <konstantin.koss@gmail.com>
2017-02-08 18:16:23 +0100

    fixed & tested user team controller

---
+++ app/concepts/api/v1/user_team/index.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+module API::V1
+  module UserTeam
+    class Index < API::V1::Default::Index
+      def base_query
+        ::UserTeam
+      end
+    end
+  end
+end
--- app/concepts/user_team/crud.rb
+++ app/concepts/user_team/crud.rb
@@ -1,29 +1,28 @@
 # frozen_string_literal: true
 class UserTeam < ActiveRecord::Base
-  class BaseOperation < Trailblazer::Operation
-    step Contract::Build()
-    step Contract::Validate()
-    step Contract::Persist()
+  class GeneralContract < Reform::Form
+    property :name
+    property :user_ids
 
-    extend Contract::DSL
-    contract do
-      property :name
-      property :user_ids
-
-      validates :name, presence: true
-      validates :user_ids, presence: true
-    end
+    validates :name, presence: true
+    validates :user_ids, presence: true
   end
 
   class Create < Trailblazer::Operation
     step Model(::UserTeam, :new)
     step Policy::Pundit(::UserTeamPolicy, :create?)
-    step Nested(UserTeam::BaseOperation)
+
+    step Contract::Build( constant: UserTeam::GeneralContract )
+    step Contract::Validate()
+    step Contract::Persist()
   end
 
   class Update < Trailblazer::Operation
     step Model(::UserTeam, :find_by)
     step Policy::Pundit(::UserTeamPolicy, :update?)
-    step Nested(UserTeam::BaseOperation)
+
+    step Contract::Build( constant: UserTeam::GeneralContract )
+    step Contract::Validate()
+    step Contract::Persist()
   end
 end
