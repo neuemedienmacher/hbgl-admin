1a99c433088474d519eb0c48ff121f16cc0d38f2
allthegoldt <damjangoldt@Damjans-MacBook-Air.local>
2017-03-06 16:54:56 +0100

    clarat-org/clarat#1030 Paginate the Pagination
    Pagination +- 3 elements and jump_to on click on …

--- client/app/bundles/Index/components/Pagination.jsx
+++ client/app/bundles/Index/components/Pagination.jsx
@@ -5,24 +5,90 @@ import PaginationCell from '../containers/PaginationCell'
 export default class Pagination extends Component {
   render() {
     const {
-      pages, previousPageHref, nextPageHref, model, params
+      pages, previousPageHref, nextPageHref, model, params, current_page, myClick
     } = this.props
 
     return (
       <div className='pagination'>
         <ul>
           {this.renderDirectionCell('left', 'previous', previousPageHref)}
-          {pages.map(page => {
-            return(
-              <PaginationCell
-                key={page} page={page} model={model} params={params}
-              />
-            )
-          })}
+          {this.renderFirstPageLogic(current_page, model, params, pages)}
+          {this.renderFirstPageSeparator(current_page, myClick)}
+          {this.renderPrePageLogic(current_page, model, params, pages, 4)}
+          {this.renderPrePageLogic(current_page, model, params, pages, 3)}
+          {this.renderPrePageLogic(current_page, model, params, pages, 2)}
+            <PaginationCell
+              key={pages[current_page - 1]} page={pages[current_page - 1]} model={model} params={params}
+            />
+          {this.renderPostPageLogic(current_page, model, params, pages, 0)}
+          {this.renderPostPageLogic(current_page, model, params, pages, 1)}
+          {this.renderPostPageLogic(current_page, model, params, pages, 2)}
+          {this.renderLastPageSeparator(current_page, pages, myClick)}
+          {this.renderLastPageLogic(current_page, model, params, pages)}
           {this.renderDirectionCell('right', 'next', nextPageHref)}
         </ul>
       </div>
     )
+  }
+
+  renderFirstPageLogic(current_page, model, params, pages) {
+    if (current_page > 4) {
+      return(
+        <PaginationCell
+          key={pages[0]} page={pages[0]} model={model} params={params}
+        />
+      )
+    }
+  }
+
+  renderFirstPageSeparator(current_page, myClick) {
+    if (current_page > 4) {
+      return(
+        <li>
+          <span onClick={myClick}>...</span>
+        </li>
+      )
+    }
+  }
+
+  renderLastPageSeparator(current_page, pages, myClick) {
+    if (current_page < pages.length - 4) {
+      return(
+        <li>
+          <span onClick={myClick}>...</span>
+        </li>
+      )
+    }
+  }
+
+  renderPrePageLogic(current_page, model, params, pages, index) {
+    if (current_page > index-1) {
+      return(
+        <PaginationCell
+          key={pages[current_page - index]} page={pages[current_page - index]} model={model} params={params}
+        />
+      )
+    }
+  }
+
+  renderPostPageLogic(current_page, model, params, pages, index) {
+    if (current_page + index < pages.length - 1 ) {
+      return(
+        <PaginationCell
+          key={pages[current_page + index]} page={pages[current_page + index]} model={model} params={params}
+        />
+      )
+    }
+  }
+
+  renderLastPageLogic(current_page, model, params, pages, index) {
+    if (current_page < pages.length ) {
+      return(
+        <PaginationCell
+          key={pages[pages.length - 1]} page={pages[pages.length - 1]} model={model} params={params}
+        />
+      )
+    }
   }
 
   renderDirectionCell(direction, className, href) {
--- client/app/bundles/Index/containers/Pagination.js
+++ client/app/bundles/Index/containers/Pagination.js
@@ -1,10 +1,15 @@
 import { connect } from 'react-redux'
+import { browserHistory } from 'react-router'
+import { encode } from 'querystring'
+import clone from 'lodash/clone'
+import merge from 'lodash/merge'
 import range from 'lodash/range'
 import Pagination from '../components/Pagination'
 
 const mapStateToProps = (state, ownProps) => {
   const resultData = state.ajax.indexResults
   const pages = range(1, resultData.meta.total_pages + 1)
+  const current_page = resultData.meta.current_page
 
   // Remove the /api/v1 part from links to make it a ui route
   let previousPageHref = resultData.links.previous || ''
@@ -16,10 +21,27 @@ const mapStateToProps = (state, ownProps) => {
     pages,
     previousPageHref,
     nextPageHref,
+    current_page
   }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Pagination)
+const mergeProps = (stateProps, dispatchProps, ownProps) => ({
+  ...stateProps,
+  ...dispatchProps,
+  ...ownProps,
+
+  myClick(event) {
+    var page = null;
+    while (stateProps.pages[page-1] == null) {
+      page = prompt("Springe zu 1-"+stateProps.pages.length+":", stateProps.current_page);
+    }
+    const params = merge(clone(ownProps.params), { page: page })
+    const href = `/${ownProps.model}?${encode(params)}`
+    browserHistory.push(href)
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Pagination)
