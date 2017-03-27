10a43b64cbe063e5a207ed1e7c9a8cbff1255b89
Nils Vollmer <nils_vollmer@gmx.de>
2017-03-27 16:32:21 +0200

    deadline-line now has max height + some refactoring

--- client/app/bundles/Statistics/components/BurnUpChart.jsx
+++ client/app/bundles/Statistics/components/BurnUpChart.jsx
@@ -73,14 +73,8 @@ export default class BurnUpChart extends React.Component {
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
     // Scale the range of the data
-    const xDomain = x.domain(extent(allData, function (d) { return d.x }))
-    const yDomain = y.domain([
-      0,
-      Math.round(Math.max(
-        max(actualData, function (d) { return d.y }),
-        max(scopeData, function (d) { return d.y })
-      ) * graphHeightFactor)
-    ])
+    const xDomain = x.domain(extent(allData, (d) => d.x))
+    const yDomain = y.domain([0, max(allData, (d) => d.y) * graphHeightFactor])
 
     // Add the axes
     svg.append('g')
@@ -105,7 +99,7 @@ export default class BurnUpChart extends React.Component {
       .attr('x1', x(idealData[1].x))
       .attr('y1', y(0))
       .attr('x2', x(idealData[1].x))
-      .attr('y2', y(max(allData, (d) => d.y)))
+      .attr('y2', y(max(allData, (d) => d.y) * graphHeightFactor))
       .attr('class', 'line deadline')
 
     actualDeadline.append('text')
