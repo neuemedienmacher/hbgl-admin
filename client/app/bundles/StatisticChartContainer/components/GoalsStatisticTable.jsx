e5e2f69835fc856a8cb4ba292b20b2777bc3e143
allthegoldt <damjangoldt@Damjans-MacBook-Air.local>
2017-03-31 17:04:01 +0200

    clarat-org/clarat#1061 Personal Statistic Table shows whole year

---
+++ client/app/bundles/StatisticChartContainer/components/GoalsStatisticTable.jsx
@@ -0,0 +1,48 @@
+import React, { PropTypes } from 'react'
+import CollapsiblePanel from '../../CollapsiblePanel/containers/CollapsiblePanel'
+
+export default class GoalsStatisticTable extends React.Component {
+  render() {
+    const { monthlySortedActualData, monthlySum,
+            titleArray, monthlyGoal } = this.props
+
+    return (
+      <table className="personal statistic table">
+        <thead>
+          <tr>
+            {titleArray.map(this.renderSingleCell.bind(this))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Ist<br></br>Soll</th>
+            {monthlySum.map(this.renderGoalsRow.bind(this))}
+          </tr>
+            {monthlySortedActualData.map(this.renderDayRow.bind(this))}
+        </tbody>
+      </table>
+    )
+  }
+
+  renderGoalsRow(sum, index){
+    return(
+    <th key={index}>{sum}<br></br>{this.props.monthlyGoal}</th>
+    )
+  }
+
+  renderDayRow(row, index){
+    return(
+    <tr key={index}>
+      <th>{index+1}.</th>
+      {row.map(this.renderSingleCell.bind(this))}
+    </tr>
+    )
+  }
+
+  renderSingleCell(row, index){
+    return(
+    <th key={index}>{row}</th>
+    )
+  }
+
+}
