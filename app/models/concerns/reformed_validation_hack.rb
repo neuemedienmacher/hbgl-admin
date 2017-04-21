2bd5ae41b5e63dc566867609f5011cbf220ea93c
Nils Vollmer <nils_vollmer@gmx.de>
2017-04-21 09:42:07 +0200

    fix for clarat-org/clarat#1102 - hacked callback before hacky validations

--- app/models/concerns/reformed_validation_hack.rb
+++ app/models/concerns/reformed_validation_hack.rb
@@ -6,6 +6,10 @@ module ReformedValidationHack
 
   included do
     before_validation do
+      # run other callbacks before validations
+      @@before_hacks[self.class.name]&.each do |func|
+        self.send(func)
+      end
       contract = hacky_contract.new(self)
       result = contract.validate(attributes)
       @errors = contract.errors
@@ -14,6 +18,14 @@ module ReformedValidationHack
 
     def hacky_contract
       self.class::Contracts::Create
+    end
+
+    def self.before_hack function
+      # rubocop:disable Style/ClassVars
+      @@before_hacks ||= {}
+      @@before_hacks[self.name] ||= []
+      @@before_hacks[self.name] << function
+      # rubocop:enable Style/ClassVars
     end
   end
 end
--- app/models/opening.rb
+++ app/models/opening.rb
@@ -4,4 +4,42 @@ require ClaratBase::Engine.root.join('app', 'models', 'opening')
 # Opening Times of Offers
 class Opening < ActiveRecord::Base
   include ReformedValidationHack
+
+  # Callbacks
+  before_hack :generate_name
+  before_hack :calculate_sort_value
+
+  def generate_name
+    self.name = concat_day_and_times
+  end
+
+  # rails_admin can only sort by a single field, that's why we are creating an
+  # imaginary time stamp that handles the sorting
+  def calculate_sort_value
+    return nil unless day
+    dummy_time = dummy_time_for_day DAYS.index(day) + 1
+    self.sort_value = (dummy_time.to_f * 100).to_i
+  end
+
+  private
+
+  def concat_day_and_times
+    if day && open && close
+      "#{day.titleize} #{open.strftime('%H:%M')}-#{close.strftime('%H:%M')}"
+    elsif day
+      "#{day.titleize} (appointment)"
+    end
+  end
+
+  # generate imaginary timestamp for a specific day
+  def dummy_time_for_day day_nr
+    if open && close
+      hour = open.hour
+      min = open.min
+      sec = close.hour + close.min / 100.0
+    else
+      hour = min = sec = 0
+    end
+    Time.zone.local(1970, 1, day_nr, hour, min, sec, 0)
+  end
 end
