722d14a21ef32cd4d4b8683cb88b605ea7fe6ae0
Nils Vollmer <nils_vollmer@gmx.de>
2017-04-25 09:56:45 +0200

    Fix: removed current_team_id again

--- test/concepts/user/representer_test.rb
+++ test/concepts/user/representer_test.rb
@@ -7,8 +7,7 @@ class API::V1::User::RepresenterTest < ActiveSupport::TestCase
   it 'should generate a generic label for a model without a name' do
     record = User.create!(
       user_team_ids: UserTeam.first.id, name: 'Nutzer', email: 'abc@bcd.de',
-      password: SecureRandom.base64, role: 'researcher',
-      current_team_id: UserTeam.first.id
+      password: SecureRandom.base64, role: 'researcher'
     )
     UserTeam.last.update(lead_id: record.id)
     result = subject.new(record).to_hash
