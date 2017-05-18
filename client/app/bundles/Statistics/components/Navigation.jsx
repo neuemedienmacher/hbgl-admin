40b61b30a27346059befb5a7f0109041504a079b
Konstantin K <konstantin.koss@gmail.com>
2017-05-18 18:06:06 +0200

    for clarat-org/clarat#944 & clarat-org/clarat#945 - more dash fixes

--- client/app/bundles/Statistics/components/Navigation.jsx
+++ client/app/bundles/Statistics/components/Navigation.jsx
@@ -11,29 +11,29 @@ export default class Navigation extends React.Component {
     return (
       <div className='list-group'>
         <Link to='/statistics' className='list-group-item'> Übersicht </Link>
-        <Link to='/statistics/offer_overview' className='list-group-item '>
+        <Link to='/statistics/offer-overview' className='list-group-item '>
           Angebotsübersicht
         </Link>
-        <Link to='/statistics/organization_overview' className='list-group-item'>
+        <Link to='/statistics/organization-overview' className='list-group-item'>
           Orga-Übersicht
         </Link>
-        <Link to='/statistics/ratio_overview' className='list-group-item'>
+        <Link to='/statistics/ratio-overview' className='list-group-item'>
           Angebote pro Orgas
         </Link>
         {/*
-        <Link to='/statistics/offer_created' className='list-group-item'>
+        <Link to='/statistics/offer-created' className='list-group-item'>
           Angebote Erstellt
         </Link>
-        <Link to='/statistics/offer_approved' className='list-group-item'>
+        <Link to='/statistics/offer-approved' className='list-group-item'>
           Angebote Approved
         </Link>
-        <Link to='/statistics/organization_created' className='list-group-item'>
+        <Link to='/statistics/organization-created' className='list-group-item'>
           Organisationen Erstellt
         </Link>
-        <Link to='/statistics/organization_approved' className='list-group-item'>
+        <Link to='/statistics/organization-approved' className='list-group-item'>
           Organisationen Approved
         </Link>
-        <Link to='/statistics/statistic_charts' className='list-group-item'>
+        <Link to='/statistics/statistic-charts' className='list-group-item'>
           Produktivitätsziele
         </Link>
         */}
