7407630a00ba8cd9be1848194579337c238cefeb
Nils Vollmer <nils_vollmer@gmx.de>
2017-06-23 09:33:30 +0200

    fixed typo

--- client/app/Backend/reducers/index.jsx
+++ client/app/Backend/reducers/index.jsx
@@ -6,7 +6,7 @@ import form from './formReducer'
 import { reducer as rform, initialState as initialRformState } from 'rform'
 import ajax from './loadAjaxDataReducer'
 import entities from './entityReducer'
-import flashMessages from './FlashMessagesReducer'
+import flashMessages from './flashMessagesReducer'
 import filteringSelect
   from '../../bundles/FilteringSelect/reducers/filteringSelectReducer'
 import statistics
