d9162dbeb3b11a3d17d6dcbb38ded47e88f094c7
Nils Vollmer <nils_vollmer@gmx.de>
2017-09-27 16:34:38 +0200

    Potential fix for offer state-changes

--- app/concepts/offer/update.rb
+++ app/concepts/offer/update.rb
@@ -24,6 +24,32 @@ class Offer::Update < Trailblazer::Operation
     step ::Lib::Macros::Nested::Create :websites, ::Website::Create
     step ::Lib::Macros::Nested::Find :logic_version, ::LogicVersion
   }
+  step :change_state_side_effect # prevents persist on faulty state change
   step Contract::Persist()
   step :set_next_steps_sort_value
+  step :generate_translations!
+
+  def change_state_side_effect(options, model:, params:, **)
+    commit = params['meta'] && params['meta']['commit']
+    return true unless commit && triggerable_event?(model, commit)
+    result = ::Offer::ChangeState.({ id: model.id }, 'event' => commit)
+    # add errors of side-effect operation to the errors of this operation
+    if result.success?
+      options['changed_state'] = true
+      options['model'] = result['model']
+    else
+      add_all_errors(result['contract.default'], options['contract.default'])
+    end
+    result.success?
+  end
+
+  def generate_translations!(opts, changed_state: false, model:, params:, **)
+    changes = opts['contract.default'].changed
+    fields = model.translated_fields.select { |f| changes[f.to_s] }
+    meta = params['meta'] && params['meta']['commit']
+    if (meta.to_s == 'approve' && changed_state) || fields.any?
+      model.generate_translations! fields.any? ? fields : :all
+    end
+    true
+  end
 end
