96ceeb8d6a0d9f291ad1d9f55dfa65b80b876c5c
Nils Vollmer <nils_vollmer@gmx.de>
2017-11-15 17:19:23 +0100

    clarat-org/clarat#1422 - extensive logging in organization#update operation

--- app/concepts/organization/update.rb
+++ app/concepts/organization/update.rb
@@ -10,8 +10,11 @@ class Organization::Update < Trailblazer::Operation
     approve: Organization::Contracts::Approve,
     else: Organization::Contracts::Update
   )
+  step :epic_logging1
   step Contract::Build()
+  step :epic_logging2
   step Contract::Validate()
+  step :epic_logging3
   step Wrap(::Lib::Transaction) {
     step ::Lib::Macros::Nested::Create :website, Website::Create
     step ::Lib::Macros::Nested::Create :divisions, Division::Create
@@ -20,6 +23,7 @@ class Organization::Update < Trailblazer::Operation
     step ::Lib::Macros::Nested::Find :umbrella_filters, ::UmbrellaFilter
     step ::Lib::Macros::Nested::Find :topics, ::Topic
   }
+  step :epic_logging4
   step Contract::Persist()
   # step ::Lib::Macros::Debug::Breakpoint()
   step :change_state_side_effect # prevents persist on faulty state change
@@ -27,6 +31,19 @@ class Organization::Update < Trailblazer::Operation
   step :assign_to_system_on_approve
   step :syncronize_done_state
   step :generate_translations!
+
+  def epic_logging1(options, **)
+    Rails.logger.debug "DEBUGGING Contract: #{options.inspect}"
+  end
+  def epic_logging2(options, **)
+    Rails.logger.debug "DEBUGGING Build: #{options.inspect}"
+  end
+  def epic_logging3(options, **)
+    Rails.logger.debug "DEBUGGING Validate: #{options.inspect}"
+  end
+  def epic_logging4(options, **)
+    Rails.logger.debug "DEBUGGING Transaction: #{options.inspect}"
+  end
 
   def change_state_side_effect(options, model:, params:, **)
     commit = params['meta'] && params['meta']['commit']
