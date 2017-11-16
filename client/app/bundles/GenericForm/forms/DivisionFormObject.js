732241f4d5a9089c95ff48dbf7ac3ade696c1ad0
allthegoldt <damjangoldt@Damjans-MacBook-Air.local>
2017-11-16 16:40:58 +0100

    form changes

--- client/app/bundles/GenericForm/forms/DivisionFormObject.js
+++ client/app/bundles/GenericForm/forms/DivisionFormObject.js
@@ -1,7 +1,9 @@
 import GenericFormObject from '../lib/GenericFormObject'
 import WebsiteFormObject from './WebsiteFormObject'
+import merge from 'lodash/merge'
+import concat from 'lodash/concat'
 
-class DivisionCreateFormObject extends GenericFormObject {
+class DivisionUpdateFormObject extends GenericFormObject {
   static get model() {
     return 'division'
   }
@@ -10,64 +12,6 @@ class DivisionCreateFormObject extends GenericFormObject {
     return 'divisions'
   }
 
-  static get properties() {
-    return [
-      'addition', 'organization', 'section', 'city', 'area', 'websites',
-      'presumed-tags', 'presumed-solution-categories', 'comment',
-      'size'
-    ]
-  }
-
-  static get submodels() {
-    return [
-      'organization', 'websites', 'section', 'city', 'area',
-      'presumed-tags', 'presumed-solution-categories'
-    ]
-  }
-
-  static get submodelConfig() {
-    return {
-      websites: { relationship: 'oneToMany', object: WebsiteFormObject },
-      section: { relationship: 'oneToOne' },
-      city: { relationship: 'oneToOne' },
-      area: { relationship: 'oneToOne' },
-      organization: { relationship: 'oneToOne' },
-      'presumed-tags': { relationship: 'oneToMany' },
-      'presumed-solution-categories': { relationship: 'oneToMany' },
-    }
-  }
-
-  static get formConfig() {
-    return {
-      addition: { type: 'string' },
-      organization: { type: 'filtering-select' },
-      section: { type: 'filtering-select' },
-      city: { type: 'filtering-select' },
-      area: { type: 'filtering-select' },
-      websites: { type: 'creating-multiselect' },
-      'presumed-tags': {
-        type: 'filtering-multiselect', resource: 'tags'
-      },
-      'presumed-solution-categories': {
-        type: 'filtering-multiselect', resource: 'solution-categories'
-      },
-      comment: { type: 'textarea' },
-      size: { type: 'select', options: ['small', 'medium', 'large'] },
-    }
-  }
-
-  static get requiredInputs() {
-    return ['section']
-  }
-
-  validation() {
-    for (let requiredInput of this.constructor.requiredInputs) {
-      this.required(requiredInput).filled()
-    }
-  }
-}
-
-class DivisionUpdateFormObject extends DivisionCreateFormObject {
   static get properties() {
     return [
       'addition', 'organization', 'city', 'area', 'websites',
@@ -112,12 +56,48 @@ class DivisionUpdateFormObject extends DivisionCreateFormObject {
     }
   }
 
+  static get readOnlyProperties() {
+    return ['section-identifier']
+  }
+
   static get requiredInputs() {
     return []
   }
 
+  validation() {
+    for (let requiredInput of this.constructor.requiredInputs) {
+      this.required(requiredInput).filled()
+    }
+  }
+}
+
+class DivisionCreateFormObject extends DivisionUpdateFormObject {
+  static get properties() {
+    return concat( DivisionUpdateFormObject.properties, ['section'])
+  }
+
+  static get submodels() {
+    return concat( DivisionUpdateFormObject.submodels, ['section'])
+  }
+
+  static get submodelConfig() {
+    return merge(DivisionUpdateFormObject.submodelConfig,
+      { section: { relationship: 'oneToOne' } }
+    )
+  }
+
+  static get formConfig() {
+    return merge(DivisionUpdateFormObject.formConfig,
+      { section: { type: 'filtering-select' } }
+    )
+  }
+
+  static get requiredInputs() {
+    return ['section']
+  }
+
   static get readOnlyProperties() {
-    return ['section-identifier']
+    return []
   }
 }
 
