0f2787cffb1292255773cb3de00ba5997f7e48ed
Nils Vollmer <nils_vollmer@gmx.de>
2017-11-22 17:47:01 +0100

    Bugfix: make duplicate work again

--- client/app/bundles/Duplicate/components/Duplicate.jsx
+++ client/app/bundles/Duplicate/components/Duplicate.jsx
@@ -3,14 +3,12 @@ import LoadingForm from '../../GenericForm/containers/LoadingForm'
 
 export default class Duplicate extends Component {
   render() {
-    const {
-      model, id, heading, modifySeedData, formStateDidMount, location
-    } = this.props
+    const { model, id, heading, modifySeedData, formStateDidMount } = this.props
 
     return(
       <div className='Duplicate'>
         <LoadingForm forceCreate
-          modifySeedData={modifySeedData} model={model} editId={id}
+          modifySeedData={modifySeedData} model={model} id={id}
           formStateDidMount={formStateDidMount} formIdSpecification='duplicate'
         />
       </div>
