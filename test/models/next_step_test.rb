131bd3a614a2ec0b7994cf1cd9c453cb0017eb91
Nils Vollmer <nils_vollmer@gmx.de>
2018-01-29 11:46:03 +0100

    removed everything related to gengo

--- test/models/next_step_test.rb
+++ test/models/next_step_test.rb
@@ -5,30 +5,4 @@ require_relative '../test_helper'
 describe NextStep do
   let(:next_step) { NextStep.new }
   subject { next_step }
-
-  describe 'callbacks' do
-    describe 'after_save' do
-      it 'should request new translations when saved for the first time' do
-        next_step = FactoryGirl.build(:next_step)
-        GengoCommunicator.any_instance.expects(:create_translation_jobs)
-                         .with(next_step, 'text')
-        next_step.save!
-      end
-
-      it 'should request new translations when text_en has changed' do
-        next_step = FactoryGirl.create(:next_step)
-        next_step.text_en = 'hi'
-        GengoCommunicator.any_instance.expects(:create_translation_jobs)
-                         .with(next_step, 'text')
-        next_step.save!
-      end
-
-      it 'wont request new translations when text_en has not changed' do
-        next_step = FactoryGirl.create(:next_step)
-        GengoCommunicator.any_instance.expects(:create_translation_jobs).never
-        next_step.text_fr = 'salut'
-        next_step.save!
-      end
-    end
-  end
 end
--- test/support/modules/webmock_stubs.rb
+++ test/support/modules/webmock_stubs.rb
@@ -2,18 +2,7 @@ module WebStubs
   ### --- GENERAL --- ###
 
   def self.enable
-    enable_gengo_stub
     enable_empty_algolia_response
-  end
-
-  ### --- GENGO --- ###
-
-  def self.enable_gengo_stub
-    WebMock.stub_request(
-      :post, 'http://api.sandbox.gengo.com/v2/translate/jobs'
-    ).to_return(
-      status: 200, body: '{"opstat": "ok", "response":{"order_id":"1"}}'
-    )
   end
 
   ### --- ALGOLIA --- ###
