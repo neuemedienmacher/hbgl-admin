660ef5c0699912d1f095969600d52d1f809bce5b
Nils Vollmer <nils.vollmer@quandoo.com>
2019-01-29 20:28:34 +0100

    final mailing text changes, cleanup and removed logo

--- app/mailers/tos_mailer.rb
+++ app/mailers/tos_mailer.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class TosMailer < ActionMailer::Base
-  add_template_helper(EmailHelper)
 
   # Informing email addresses about the ToS, asking for approval or denial
   # @attr email Email object this is sent to
@@ -9,7 +8,6 @@ class TosMailer < ActionMailer::Base
     # Loads of variables in preparation for view models (email view)
     # @offers = email.offers.visible_in_frontend
     @offers = email.offers
-    @overview_href = "https://local.handbookgermany.de/emails/#{email.id}/angebote" # TODO? optional: anchor link?
     @tos_href = "https://local.handbookgermany.de/emails/#{email.id}/angebote"
     @reply_date = date.strftime("%d.%m.%Y")
 
@@ -21,7 +19,7 @@ class TosMailer < ActionMailer::Base
   def send_tos_email email, offers
     email.create_tos_mailings offers
     mail subject: 'Ihre Aufnahme in Deutschlands größte Datenbank für soziale Organisationen',
-         from: 'Sabine Roßmann <localsearch@neuemedienmacher.de>',
+         from: 'Sabine Roßmann <localsearch@handbookgermany.de>',
          to: email.address
   end
 end
