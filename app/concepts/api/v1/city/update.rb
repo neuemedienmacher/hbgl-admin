b3e2eb09a64e3b02bbf81241ab35d9539655312f
Andreas Rotter <andreas.rotter.kontakt@googlemail.com>
2021-06-09 17:42:30 +0200

    use distinct representer for update

--- app/concepts/api/v1/city/update.rb
+++ app/concepts/api/v1/city/update.rb
@@ -2,7 +2,7 @@ module API::V1
   module City
     class Update < ::City::Update
       extend Trailblazer::Operation::Representer::DSL
-      representer API::V1::City::Representer::Create
+      representer API::V1::City::Representer::Update
     end
   end
 end
