83b7f204c0d415d73f1ce1efd98ab589417a7f7e
Andreas Rotter <andreas.rotter.kontakt@googlemail.com>
2021-06-09 18:06:02 +0200

    fix lint errors

--- client/app/bundles/GenericForm/containers/Standalone.jsx
+++ client/app/bundles/GenericForm/containers/Standalone.jsx
@@ -1,20 +1,18 @@
-import { connect } from 'react-redux'
-import parseLocation from '../lib/parseLocation'
-import Standalone from '../components/Standalone'
+import { connect } from "react-redux";
+import parseLocation from "../lib/parseLocation";
+import Standalone from "../components/Standalone";
 
 const mapStateToProps = (state, ownProps) => {
-  const [ model, idOrNew, edit ] = parseLocation(ownProps)
-  const id = edit ? idOrNew : null
+  const [model, idOrNew, edit] = parseLocation(ownProps);
+  const id = edit ? idOrNew : null;
 
   return {
     model,
     id,
-    location: ownProps.location
-  }
-}
+    location: ownProps.location,
+  };
+};
 
-const mapDispatchToProps = (dispatch, ownProps) => ({ })
-
-export default connect(mapStateToProps, mapDispatchToProps)(
+export default connect(mapStateToProps)(
   Standalone
-)
+);
