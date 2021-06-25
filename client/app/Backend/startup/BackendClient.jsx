bc4c5a8134f8e09bc0b7c47e3f14e3333bb840f0
Andreas Rotter <andreas.rotter.kontakt@googlemail.com>
2021-06-25 10:47:16 +0200

    add react-query provider

--- client/app/Backend/startup/BackendClient.jsx
+++ client/app/Backend/startup/BackendClient.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { QueryClientProvider, QueryClient } from "react-query";
 
 import getStore from "../store/store";
 import Routes from "../containers/Routes";
 
+const queryClient = new QueryClient();
+
 export default props => {
   const store = getStore(props);
-  console.log(props);
+
   return (
-    <Provider store={store}>
-      <Routes />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </QueryClientProvider>
   );
 };
