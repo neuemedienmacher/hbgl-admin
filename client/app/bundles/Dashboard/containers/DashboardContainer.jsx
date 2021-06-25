431a81e12d16463977467892575b2a07f0fd2c9a
Andreas Rotter <andreas.rotter.kontakt@googlemail.com>
2021-06-25 10:53:09 +0200

    simplify code

--- client/app/bundles/Dashboard/containers/DashboardContainer.jsx
+++ client/app/bundles/Dashboard/containers/DashboardContainer.jsx
@@ -1,15 +1,8 @@
-import { connect } from 'react-redux'
-import Dashboard from '../components/Dashboard'
+import { connect } from "react-redux";
+import Dashboard from "../components/Dashboard";
 
-const mapStateToProps = (state, ownProps) => {
-  // read current_user from users with current_user.id (current_user not updated)
-  const user = state.entities.users[state.entities['current-user-id']]
+const mapStateToProps = state => ({
+  user: state.entities.users[state.entities["current-user-id"]]
+});
 
-  return {
-    user
-  }
-}
-
-const mapDispatchToProps = (dispatch, ownProps) => ({})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
+export default connect(mapStateToProps)(Dashboard);
