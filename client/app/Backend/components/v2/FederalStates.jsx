ef5de9322eb2f30d74dba03291ea81f995455162
Andreas Rotter <andreas.rotter.kontakt@googlemail.com>
2021-11-05 14:04:52 +0100

    first draft for solution with react table

--- client/app/Backend/components/v2/FederalStates.jsx
+++ client/app/Backend/components/v2/FederalStates.jsx
@@ -1,30 +1,15 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { useQuery } from "react-query";
 import ReactTable from "react-table-v6";
+import { ButtonToolbar, ButtonGroup, Button } from "react-bootstrap";
 import { api } from "../../constants/routes";
 
 import "react-table-v6/react-table.css";
 
-const columns = [
-  {
-    id: "checkbox",
-    accessor: "",
-    Cell: row => (<input type="checkbox" />),
-    minWidth: 100
-  },
-  {
-    Header: "Id",
-    accessor: "id",
-    defaultSortDesc: false
-  },
-  {
-    Header: "Name",
-    accessor: "label",
-    filterable: true
-  },
-];
 
 const FederalStates = () => {
+  const [checkedRows, setCheckedRows, ] = useState([]);
+
   const { isLoading, isError, data, error } = useQuery("fetchFederalStates", async () => {
     const res = await fetch(api.federalStates);
 
@@ -33,6 +18,31 @@ const FederalStates = () => {
     }
     return await res.json();
   });
+
+  const columns = [
+    {
+      id: "checkbox",
+      accessor: "",
+      Cell: ({ original }) => (<input type="checkbox" onClick={e => {
+        if (e.currentTarget.checked) {
+          setCheckedRows(checkedRows.concat([original.id, ]));
+          return;
+        }
+        setCheckedRows(checkedRows.filter(row => row !== original.id));
+      }} />),
+      maxWidth: 42
+    },
+    {
+      Header: "Id",
+      accessor: "id",
+      defaultSortDesc: false
+    },
+    {
+      Header: "Name",
+      accessor: "label",
+      filterable: true
+    },
+  ];
 
   if (isLoading) {
     return (<div>schön am laden hier</div>);
@@ -50,11 +60,20 @@ const FederalStates = () => {
   const tableData = data.data.map(state => ({ id: Number(state.id), label: state.attributes.label }));
 
   return (
-    <ReactTable
-      columns={columns}
-      data={tableData}
-      showPagination={tableData.length > 20}
-    />);
+    <Fragment>
+      <ButtonToolbar>
+        <ButtonGroup>
+          <Button disabled={true}>Hinzufügen</Button>
+          <Button disabled={true}>Bearbeiten</Button>
+          <Button disabled={checkedRows.length === 0}>Entfernen</Button>
+        </ButtonGroup>
+      </ButtonToolbar>
+      <ReactTable
+        columns={columns}
+        data={tableData}
+        showPagination={tableData.length > 20}
+      />
+    </Fragment>);
 };
 
 export default FederalStates;
